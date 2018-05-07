68a125a316a39c94cdce41ae694d86e15130a97f
WOODY\Woody <yaroslav.dyatel@gmail.com>
2018-05-07 17:35:57 +0200

    Decorator pattern based on cafe (beverages)

---
+++ Patterns_DecoratorPattern/BeveragesMain/BeveragesBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns_DecoratorPattern.BeveragesMain {
+	public class BeveragesBase {
+
+	}
+}
---
+++ Patterns_DecoratorPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Patterns_DecoratorPattern.BeveragesMain;
+
+namespace Patterns_DecoratorPattern {
+	class Program {
+		static void Main (string[] args) {
+
+		}
+	}
+}
